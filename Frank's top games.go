0c3a4828a28ab9ad3e1335a4b0bf1ffdc760d518
fnanez001 <fnanez001@student.butte.edu>
2020-04-25 18:12:42 +0000

    building struct and objects?

--- Frank's top games.go
+++ Frank's top games.go
@@ -1,3 +1,196 @@
 // Frank Nanez
-//
-//
+// 4-25-20
+// program to display my top 25 games played on steam and give information for user to learn more about the game.
+
+package main
+
+import main
+
+type ginfo struct {
+  Number:  int
+  Name, Hrs, Genre, Synopsis string
+
+}
+
+func game(g[25] int)  {
+  g[0] := {
+    Number: 1,
+    Name: "Banished",
+    Hrs: 349,
+    Genre: "Simulation", "Stragey", "City Builder",
+    Sypopsis: "You and a group of exiled travelers who decide to restart their lives in a new land. They have only the clothes on their backs and a cart filled with supplies from their homeland. The townspeople of Banished are your primary resource. You must build, survive, and if you’re good enough, thrive.",
+  }
+  g[1] := {
+    Number: 2,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[2] := {
+    Number: 3,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[3] := {
+    Number: 4,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[4] := {
+    Number: 5,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[5] := {
+    Number: 6,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[6] := {
+    Number: 7,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[7] := {
+    Number: 8,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[8] := {
+    Number: 9,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[9] := {
+    Number: 10,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[10] := {
+    Number: 11,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[11] := {
+    Number: 12,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }g[12] := {
+    Number: 13,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[13] := {
+    Number: 14,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }g[14] := {
+    Number: 15,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[15] := {
+    Number: 16,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[16] := {
+    Number: 17,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[17] := {
+    Number: 18,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[18] := {
+    Number: 19,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[19] := {
+    Number: 20,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[20] := {
+    Number: 21,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[21] := {
+    Number: 22,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[22] := {
+    Number: 23,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[23] := {
+    Number: 24,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+  g[24] := {
+    Number: 25,
+    Name:
+    Hrs: 
+    Grnre:
+    Synopsis:
+  }
+}
+
+func main (){
+  
+  var x int
+
+
+}
