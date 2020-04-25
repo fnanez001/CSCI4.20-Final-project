a9d1cbc5b7cc8b4fbca03549e39a155cc1ebbb24
fnanez001 <fnanez001@student.butte.edu>
2020-04-25 18:39:05 +0000

    attempting to get function to pull correct info.

--- Franks top games.go
+++ Franks top games.go
@@ -12,7 +12,8 @@ type ginfo struct {
 
 }
 
-func game(g[25] int)  {
+func game(g[i] int) int, string, int, string, string {
+  var i int
   g[0] := {
     Number: 1,
     Name: "Banished",
@@ -24,173 +25,185 @@ func game(g[25] int)  {
     Number: 2,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[2] := {
     Number: 3,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[3] := {
     Number: 4,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[4] := {
     Number: 5,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[5] := {
     Number: 6,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[6] := {
     Number: 7,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[7] := {
     Number: 8,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[8] := {
     Number: 9,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[9] := {
     Number: 10,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[10] := {
     Number: 11,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[11] := {
     Number: 12,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
-  }g[12] := {
+  }
+  g[12] := {
     Number: 13,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[13] := {
     Number: 14,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
-  }g[14] := {
+  }
+  g[14] := {
     Number: 15,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[15] := {
     Number: 16,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[16] := {
     Number: 17,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[17] := {
     Number: 18,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[18] := {
     Number: 19,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[19] := {
     Number: 20,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[20] := {
     Number: 21,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[21] := {
     Number: 22,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[22] := {
     Number: 23,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[23] := {
     Number: 24,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
   g[24] := {
     Number: 25,
     Name:
     Hrs: 
-    Grnre:
+    Genre:
     Synopsis:
   }
+  return g[i] Number, Name, Hrs, Genre, Synopsis
 }
 
 func main (){
   
-  var x int
+  x := 0
+
+  for x != "Done" OR x != "done" OR x != "DONE"
+    for i = 0, i <= 25, i++{
+      x = game(g[x].Number,game(g[x].Name))
+      fmt.Println(game(g[x].Name, game(g[x].Name)))
+      x = x+1
+    }
+
+  
 
 
 }
