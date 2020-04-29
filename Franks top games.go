26efc8fdc20058a75b8fec3441f557e6c2da35f5
fnanez001 <fnanez001@student.butte.edu>
2020-04-29 19:08:31 +0000

    updated code

--- Franks top games.go
+++ Franks top games.go
@@ -4,192 +4,194 @@
 
 package main
 
-import main
+import "fmt"
 
 type ginfo struct {
-  Number:  int
+  Number  int
   Name, Hrs, Genre, Synopsis string
 
 }
 
-func game(g[i] int) int, string, int, string, string {
-  var i int
-  g[0] := {
+func game(i int) (int, string, int, string, string) {
+
+  var n int
+
+  Game[0] := {
     Number: 1,
     Name: "Banished",
     Hrs: 349,
     Genre: "Simulation", "Stragey", "City Builder",
-    Sypopsis: "You and a group of exiled travelers who decide to restart their lives in a new land. They have only the clothes on their backs and a cart filled with supplies from their homeland. The townspeople of Banished are your primary resource. You must build, survive, and if you’re good enough, thrive.",
+    Synopsis: "You and a group of exiled travelers who decide to restart their lives in a new land. They have only the clothes on their backs and a cart filled with supplies from their homeland. The townspeople of Banished are your primary resource. You must build, survive, and if you’re good enough, thrive.",
   }
-  g[1] := {
+  G[1] := {
     Number: 2,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[2] := {
+  G[2] := {
     Number: 3,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[3] := {
+  G[3] := {
     Number: 4,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[4] := {
+  G[4] := {
     Number: 5,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[5] := {
+  G[5] := {
     Number: 6,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[6] := {
+  G[6] := {
     Number: 7,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[7] := {
+  G[7] := {
     Number: 8,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[8] := {
+  G[8] := {
     Number: 9,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[9] := {
+  G[9] := {
     Number: 10,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[10] := {
+  G[10] := {
     Number: 11,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[11] := {
+  G[11] := {
     Number: 12,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[12] := {
+  G[12] := {
     Number: 13,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[13] := {
+  G[13] := {
     Number: 14,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[14] := {
+  G[14] := {
     Number: 15,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[15] := {
+  G[15] := {
     Number: 16,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[16] := {
+  G[16] := {
     Number: 17,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[17] := {
+  G[17] := {
     Number: 18,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[18] := {
+  G[18] := {
     Number: 19,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[19] := {
+  G[19] := {
     Number: 20,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[20] := {
+  G[20] := {
     Number: 21,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[21] := {
+  G[21] := {
     Number: 22,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[22] := {
+  G[22] := {
     Number: 23,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[23] := {
+  G[23] := {
     Number: 24,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  g[24] := {
+  G[24] := {
     Number: 25,
     Name:
     Hrs: 
     Genre:
     Synopsis:
   }
-  return g[i] Number, Name, Hrs, Genre, Synopsis
+  return G[n] Number, Name, Hrs, Genre, Synopsis
 }
 
 func main (){
@@ -199,7 +201,7 @@ func main (){
   for x != "Done" OR x != "done" OR x != "DONE"
     for i = 0, i <= 25, i++{
       x = game(g[x].Number,game(g[x].Name))
-      fmt.Println(game(g[x].Name, game(g[x].Name)))
+      fmt.Println(game(g[x].Number, game(g[x].Name)))
       x = x+1
     }
 
